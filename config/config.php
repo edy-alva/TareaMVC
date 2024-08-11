d10eba9cf0cc80891a2cc9375b2ffd0c1ab77a7d
edy-alva <edy.alva.pagos@gmail.com>
2024-08-11 12:34:57 -0500

    Carga de Desarrollo CRUD
    Elaboración de Modelos y Controladores

--- config/config.php
+++ config/config.php
@@ -6,11 +6,12 @@ class ClaseConectar
     protected $db;
     private $host = "localhost";
     private $usuario = "root";
-    private $pass = "root";
+    private $pass = "";
     private $base = "sexto";
+    private $puerto = "3386";
     public function ProcedimientoParaConectar()
     {
-        $this->conexion = mysqli_connect($this->host, $this->usuario, $this->pass, $this->base);
+        $this->conexion = mysqli_connect($this->host, $this->usuario, $this->pass, $this->base,$this->puerto);
         mysqli_query($this->conexion, "SET NAMES 'utf8'");
         if ($this->conexion->connect_error) {
             die("Error al conectar con el servidor: " . $this->conexion->connect_error);
